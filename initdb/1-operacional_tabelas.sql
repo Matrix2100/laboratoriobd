b5f2f9681c1fdae487969b3243195f3ec619d4c1
Natália Fonseca <nataliabf@outlook.com>
2022-06-11 01:42:49 -0300

    etl

--- initdb/1-operacional_tabelas.sql
+++ initdb/1-operacional_tabelas.sql
@@ -1,15 +1,15 @@
-CREATE TABLE departamentos (
+CREATE TABLE  IF NOT EXISTS departamentos (
     cod_dpto    INT NOT NULL PRIMARY KEY,
     nome_dpto   VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE cursos (
+CREATE TABLE  IF NOT EXISTS cursos (
     cod_curso   INT NOT NULL PRIMARY KEY,
     nom_curso   VARCHAR(80) NOT NULL,
     cod_dpto    INT NOT NULL REFERENCES departamentos
 );
 
-CREATE TABLE alunos (
+CREATE TABLE  IF NOT EXISTS alunos (
     mat_alu       INT NOT NULL PRIMARY KEY,
     nome          VARCHAR(100) NOT NULL,
     dat_entrada   DATE NOT NULL,
@@ -17,13 +17,13 @@ CREATE TABLE alunos (
     cotista       CHAR(1) NOT NULL
 );
 
-CREATE TABLE disciplinas (
+CREATE TABLE  IF NOT EXISTS disciplinas (
     cod_disc        INT NOT NULL PRIMARY KEY,
     nome_disc       VARCHAR(60) NOT NULL,
     carga_horaria   NUMERIC NOT NULL
 );
 
-CREATE TABLE matriculas (
+CREATE TABLE  IF NOT EXISTS matriculas (
     semestre   INT NOT NULL,
     mat_alu    INT NOT NULL REFERENCES alunos,
     cod_disc   INT NOT NULL REFERENCES disciplinas,
@@ -33,7 +33,7 @@ CREATE TABLE matriculas (
   	PRIMARY KEY (mat_alu, semestre)
 );
 
-CREATE TABLE matrizes_cursos (
+CREATE TABLE  IF NOT EXISTS matrizes_cursos (
     cod_curso   INT NOT NULL REFERENCES cursos,
     cod_disc    INT NOT NULL REFERENCES disciplinas,
     periodo     INT NOT NULL,
--- initdb/8-dimensional.sql
+++ initdb/8-dimensional.sql
@@ -2,30 +2,30 @@ CREATE DATABASE dimensional;
 
 \c dimensional;
 
-CREATE TABLE dm_departamento (
+CREATE TABLE IF NOT EXISTS dm_departamentos (
         id_dpto varchar NOT NULL PRIMARY KEY,
         nome_dpto varchar NOT NULL
 );
 
-CREATE TABLE dm_disciplina (
+CREATE TABLE IF NOT EXISTS dm_disciplinas (
         id_disc varchar  NOT NULL PRIMARY KEY,
         nome_disc varchar NOT NULL,
         carga_horaria varchar NOT NULL
 );
 
-CREATE TABLE dm_curso (
+CREATE TABLE IF NOT EXISTS dm_cursos (
         id_curso varchar NOT NULL PRIMARY KEY,
         nome_curso varchar NOT NULL
 );
 
-CREATE TABLE dm_tempo (
+CREATE TABLE IF NOT EXISTS dm_tempo (
         id_tempo varchar NOT NULL PRIMARY KEY,
         semestre varchar NOT NULL
 );
 
-CREATE TABLE ft_reprovacao_cotas (
-        id_tempo varchar NOT NULL REFERENCES dm_tempo,
-        id_disc varchar NOT NULL REFERENCES dm_disciplina,
+CREATE TABLE IF NOT EXISTS ft_reprovacao_cotas (
+        id_tempo varchar NOT NULL REFERENCES dm_tempo ON DELETE CASCADE,
+        id_disc varchar NOT NULL REFERENCES dm_disciplinas ON DELETE CASCADE,
         total_reprovacoes_cotas varchar NOT NULL,
         total_matriculas_cotas varchar NOT NULL,
         total_reprovacoes varchar NOT NULL,
@@ -33,11 +33,11 @@ CREATE TABLE ft_reprovacao_cotas (
         PRIMARY KEY (id_tempo, id_disc)
 );
 
-CREATE TABLE ft_reprovacao (
-        id_tempo varchar NOT NULL REFERENCES dm_tempo,
-        id_disc varchar NOT NULL REFERENCES dm_disciplina,
-        id_curso varchar NOT NULL REFERENCES dm_curso,
-        id_dpto varchar NOT NULL REFERENCES dm_departamento,
+CREATE TABLE IF NOT EXISTS ft_reprovacao (
+        id_tempo varchar NOT NULL REFERENCES dm_tempo ON DELETE CASCADE,
+        id_disc varchar NOT NULL REFERENCES dm_disciplinas ON DELETE CASCADE,
+        id_curso varchar NOT NULL REFERENCES dm_cursos ON DELETE CASCADE,
+        id_dpto varchar NOT NULL REFERENCES dm_departamentos ON DELETE CASCADE,
         total_reprovacoes varchar NOT NULL,
         total_matriculas varchar NOT NULL,
         PRIMARY KEY (id_tempo, id_disc, id_curso, id_dpto)
